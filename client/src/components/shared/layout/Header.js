d9fbf5a14ed5203b4c94620a599c39de3a75f853
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-07-23 11:47:27 +0530

    Layout and logout function added

---
+++ client/src/components/shared/layout/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BiDonateBlood, BiUserCircle } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  //logout
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    alert("Logout Successfully");
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <nav className="navbar ">
+        <div className="container-fluid">
+          <div className="navbar-brand h1  ">
+            <BiDonateBlood color="red" /> Blood Bank App
+          </div>
+          <ul className="navbar-nav flex-row">
+            <li className="nav-item mx-3">
+              <p className="nav-link">
+                <BiUserCircle />
+                {`Welcome ${user?.name} !`}
+              </p>
+            </li>
+            <li className="nav-item mx-3 ">
+              <button onClick={handleLogout} className="btn btn-danger">
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,12 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../components/shared/Spinner";
 import { toast } from "react-toastify";
+import Layout from "../components/shared/layout/Layout";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
 
   return (
-    <>
+    <Layout>
       {error && <span>{toast.error(error)}</span>}
       {loading ? (
         <Spinner />
@@ -16,7 +17,7 @@ const HomePage = () => {
           <h1>HomePage</h1>
         </div>
       )}
-    </>
+    </Layout>
   );
 };
 
